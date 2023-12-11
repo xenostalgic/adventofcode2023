c8a8d0cee30a60160308127b8318078e6add9cf6
xenostalgic <>
2023-12-11 00:57:47 -0500

    update utils

--- utils.py
+++ utils.py
@@ -1,10 +1,77 @@
 from dataclasses import dataclass
+import os
+import re
 from typing import Any
 import numpy as np
 from functools import reduce
 from itertools import product
 
 import IPython as ipy
+
+import requests
+
+SESSION = "53616c7465645f5fe2c49c8ba182ef4c502787aa26dc8ff761c877f6a8f57f547a6424f693da66b93f476836e7a1695847f7f065702c0d806a36801e09654370"
+# SESSION = "53616c7465645f5fe2c49c8ba182ef4c502787aa26dc8ff761c877f6a8f57f547a6424f693da66b93f476836e7a1695847f7f065702c0d806a36801e09654370"
+
+def load_file(filename, year=2023, day=11):
+    if not os.path.exists(filename):
+        cookies = dict(session=SESSION)
+        r = requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies)
+        if r.status_code == 200:
+            text = r.content.decode("utf-8")
+            with open(filename, 'w') as f:
+                f.write(text)
+        else:
+            raise requests.HTTPError("Couldn't download input")
+    else:
+        with open(filename, 'r') as f:
+            text = f.read()
+    return text
+
+
+def read_grid(text):
+    return [line.strip() for line in text.splitlines()]
+
+
+def parse_lines(text):
+    entries = []
+    for line in text.splitlines():
+        counts = re.findall(r"\d+", line)
+        types = re.findall(r'\d+\s+(\w+)', line)
+        entry = {t:c for t,c in zip(types,counts)}
+        entries.append(entry)
+    return entries
+
+
+def parse_groups(text):
+    entries = []
+    for group in text.split("\n\n"):
+        glines = group.split("\n")
+        entry = {line.split(":")[0].strip():line.split(":")[1].strip() for line in glines}
+        entries.append(entry)
+    return entries
+
+ALLDIRS = [
+    (-1,-1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0,  0),
+    (0,  1),
+    (1, -1),
+    (1,  0),
+    (1,  1),
+]
+DIRNAMES = ["NW", "N", "NE", "W", "C", "E", "SW", "S", "SE"]
+NAMED_ALLDIRS = {d:step for d,step in zip(DIRNAMES, ALLDIRS)}
+
+STEPDIRS = [
+    (-1, 0), # N
+    (0, -1), # W
+    (0,  1), # E
+    (1,  0), # S
+]
+NAMED_STEPDIRS = {d:step for d,step in zip("NWES", STEPDIRS)}
 
 W2N = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
@@ -95,7 +162,7 @@ def lcm_n(nums: list[int]):
 ### SEARCH
 
 def manhattan_distance(pos1, pos2):
-    return sum((d1-d2)**2 for d1,d2 in zip(pos1,pos2))
+    return sum(abs(d1-d2) for d1,d2 in zip(pos1,pos2))
 
 def euclidian_distance(pos1, pos2):
     return np.sqrt(sum((d1-d2)**2 for d1,d2 in zip(pos1,pos2)))
@@ -135,3 +202,5 @@ def bfs(start_node: Node, tgt_nodes: list[Node]):
     return step_idx
 
 
+def tplus(p1, p2):
+    return tuple(p1[i]+p2[i] for i in range(len(p1)))
